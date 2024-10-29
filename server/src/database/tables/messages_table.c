cd06f69c07597f41535e525fdb598fe9dbd3bab8
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-10-29 14:36:05 +0200

    refactor send_message use itoa

--- server/src/database/tables/messages_table.c
+++ server/src/database/tables/messages_table.c
@@ -7,26 +7,16 @@ bool send_message(PGconn *conn, int chat_id, int sender_id, const char *message_
     const char *query =
         "INSERT INTO messages (chat_id, sender_id, message_text, media, reply_to_chat, reply_to_message, "
         "forwarded_from_chat, forwarded_from_message) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
-    const char *params[8];
     char chat_id_str[12], sender_id_str[12], media_id_str[12], reply_to_chat_str[12], reply_to_message_str[12],
         forwarded_from_chat_str[12], forwarded_from_message_str[12];
-
-    snprintf(chat_id_str, 12, "%d", chat_id);
-    snprintf(sender_id_str, 12, "%d", sender_id);
-    snprintf(media_id_str, 12, "%d", media_id);
-    snprintf(reply_to_chat_str, 12, "%d", reply_to_chat);
-    snprintf(reply_to_message_str, 12, "%d", reply_to_message);
-    snprintf(forwarded_from_chat_str, 12, "%d", forwarded_from_chat);
-    snprintf(forwarded_from_message_str, 12, "%d", forwarded_from_message);
-
-    params[0] = chat_id_str;
-    params[1] = sender_id_str;
-    params[2] = message_text;
-    params[3] = media_id ? media_id_str : NULL;
-    params[4] = reply_to_chat ? reply_to_chat_str : NULL;
-    params[5] = reply_to_message ? reply_to_message_str : NULL;
-    params[6] = forwarded_from_chat ? forwarded_from_chat_str : NULL;
-    params[7] = forwarded_from_message ? forwarded_from_message_str : NULL;
+    const char *params[8] = {itoa(chat_id, chat_id_str),
+                             itoa(sender_id, sender_id_str),
+                             message_text,
+                             media_id ? itoa(media_id, media_id_str) : NULL,
+                             reply_to_chat ? itoa(reply_to_chat, reply_to_chat_str) : NULL,
+                             reply_to_message ? itoa(reply_to_message, reply_to_message_str) : NULL,
+                             forwarded_from_chat ? itoa(forwarded_from_chat, forwarded_from_chat_str) : NULL,
+                             forwarded_from_message ? itoa(forwarded_from_message, forwarded_from_message_str) : NULL};
 
     PGresult *res = PQexecParams(conn, query, 8, NULL, params, NULL, NULL, 0);
 
@@ -35,6 +25,7 @@ bool send_message(PGconn *conn, int chat_id, int sender_id, const char *message_
         PQclear(res);
         return false;
     }
+
     PQclear(res);
     return true;
 }
