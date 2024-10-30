63f895a044c0cfb928fabde7dd8f5740592b2439
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-10-30 17:55:24 +0200

    add DatabasePool struct

--- server/inc/database_pool.h
+++ server/inc/database_pool.h
@@ -15,6 +15,26 @@ typedef struct {
     bool in_use;
 } Connection;
 
+struct DatabasePool {
+    void (*init)();
+    PGconn *(*acquire_connection)();
+    void (*release_connection)(PGconn *);
+    void (*cleanup)();
+};
+
+void init_postgres_pool();
+PGconn *acquire_connection();
+void release_connection(PGconn *conn);
+void cleanup_pool();
+
+static struct DatabasePool database_pool = {
+    .init = init_postgres_pool,
+    .acquire_connection = acquire_connection,
+    .release_connection = release_connection,
+    .cleanup = cleanup_pool
+};
+
+
 static Connection pool[POOL_SIZE];
 static pthread_mutex_t pool_mutex = PTHREAD_MUTEX_INITIALIZER;
 
