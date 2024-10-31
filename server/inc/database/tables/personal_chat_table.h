1178a165d22398306308ba1a34cd50c11cdf8272
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-11-01 01:06:45 +0200

    add doc comments

--- server/inc/database/tables/personal_chat_table.h
+++ server/inc/database/tables/personal_chat_table.h
@@ -3,15 +3,53 @@
 #pragma once
 
 #include <libpq-fe.h>
+#include <stdbool.h>
 
+/** Creates a personal chat between two users.
+ *
+ * @param conn The connection to the PostgreSQL database.
+ * @param user1_id The ID of the first user.
+ * @param user2_id The ID of the second user.
+ * @return The ID of the created chat, or -1 if the chat could not be created.
+ */
 int create_personal_chat(PGconn *conn, int user1_id, int user2_id);
 
+/** Retrieves a personal chat between two users.
+ *
+ * @param conn The connection to the PostgreSQL database.
+ * @param user1_id The ID of the first user.
+ * @param user2_id The ID of the second user.
+ * @return A PGresult containing the chat data, or NULL if the query failed.
+ *         Caller is responsible for freeing the result with PQclear.
+ */
 PGresult *get_personal_chat(PGconn *conn, int user1_id, int user2_id);
 
+/** Deletes a personal chat between two users.
+ *
+ * @param conn The connection to the PostgreSQL database.
+ * @param user1_id The ID of the first user.
+ * @param user2_id The ID of the second user.
+ * @return true if the chat was deleted successfully, false otherwise.
+ */
 bool delete_personal_chat(PGconn *conn, int user1_id, int user2_id);
 
+/** Updates the background of a personal chat between two users.
+ *
+ * @param conn The connection to the PostgreSQL database.
+ * @param user1_id The ID of the first user.
+ * @param user2_id The ID of the second user.
+ * @param media_id The ID of the media to set as the background.
+ * @return true if the background was updated successfully, false otherwise.
+ */
 bool update_background_personal_chat(PGconn *conn, int user1_id, int user2_id, int media_id);
 
+/** Lists all personal chats of a user.
+ *
+ * @param conn The connection to the PostgreSQL database.
+ * @param user_id The ID of the user.
+ * @return A PGresult containing the chat data, or NULL if the query failed.
+ *         Caller is responsible for freeing the result with PQclear.
+ */
 PGresult *list_user_personal_chats(PGconn *conn, int user_id);
 
 typedef struct {
