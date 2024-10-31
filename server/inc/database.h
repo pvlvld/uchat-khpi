ed218dbdbb736d9f4bbdf9fa5444807b8e030dcf
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-11-01 00:03:37 +0200

    init main database header

---
+++ server/inc/database.h
@@ -0,0 +1,19 @@
+#ifndef POSTGRES_H
+#define POSTGRES_H
+
+#pragma once
+
+#include "../../libraries/cJSON/cJSON.h"
+#include "database/database_pool.h"
+#include "database/tables/messages_table.h"
+#include "database/tables/personal_chat_table.h"
+#include "database/tables/users_table.h"
+
+typedef struct {
+    t_database_pool pool;
+    t_messages_table messages_table;
+    t_personal_chat_table personal_chat_table;
+    t_users_table users_table;
+} t_database;
+
+#endif // POSTGRES_H
