98bfa12559cf45997c962e806bf545c73152bef4
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-11-01 21:47:45 +0200

    seasoned with some void's

--- server/inc/database/database_pool.h
+++ server/inc/database/database_pool.h
@@ -18,21 +18,21 @@ typedef struct {
 
 typedef struct {
     /* A function pointer to initialize the database pool. */
-    void (*init)();
+    void (*init)(void);
     /* A function pointer to initialize the connections in the database pool. */
-    void (*init_connections)();
+    void (*init_connections)(void);
     /* A function pointer to acquire a connection from the database pool. */
-    PGconn *(*acquire_connection)();
+    PGconn *(*acquire_connection)(void);
     /* A function pointer to release a connection back to the database pool. */
     void (*release_connection)(PGconn *);
     /* A function pointer to close all connections in the database pool. */
-    void (*cleanup)();
+    void (*cleanup)(void);
 } t_database_pool;
 
 void init_postgres_pool(t_database_pool *database_pool);
-void init_postgres_pool_connections();
-PGconn *acquire_connection();
+void init_postgres_pool_connections(void);
+PGconn *acquire_connection(void);
 void release_connection(PGconn *conn);
-void cleanup_pool();
+void cleanup_pool(void);
 
 #endif // DATABASE_POOL_H
