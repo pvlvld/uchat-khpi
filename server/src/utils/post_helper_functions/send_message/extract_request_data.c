a367d137d99743dd15e979c72dbea394e492801a
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-12-01 19:43:23 +0200

    fix: includes & extract_chat_id errors

--- server/src/utils/post_helper_functions/send_message/extract_request_data.c
+++ server/src/utils/post_helper_functions/send_message/extract_request_data.c
@@ -1,5 +1,6 @@
-#include "utils.h"
-#include <../../../libraries/cJSON/cJSON.h>
+#include "../../../../../libraries/cJSON/cJSON.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 char *strdup(const char *src) {
@@ -9,48 +10,34 @@ char *strdup(const char *src) {
     return dst;
 }
 
-
 char *extract_recipient_login(cJSON *json) {
-    if (!json) {
-        return NULL;
-    }
+    if (!json) { return NULL; }
 
     cJSON *recipient_login_item = cJSON_GetObjectItem(json, "recipient_login");
-    if (!cJSON_IsString(recipient_login_item)) {
-        return NULL;
-    }
+    if (!cJSON_IsString(recipient_login_item)) { return NULL; }
 
     // Duplicate the recipient login string to return (remember to free it later)
     return strdup(recipient_login_item->valuestring);
 }
 
 char *extract_message(cJSON *json) {
-    if (!json) {
-        return NULL;
-    }
+    if (!json) { return NULL; }
 
     // Get the message field
     cJSON *message_item = cJSON_GetObjectItem(json, "message");
-    if (!cJSON_IsString(message_item)) {
-        return NULL;
-    }
+    if (!cJSON_IsString(message_item)) { return NULL; }
 
     // Duplicate the message string to return (remember to free it later)
     return strdup(message_item->valuestring);
 }
 
 char *extract_chat_id(cJSON *json) {
-    if (!json) {
-        return NULL;
-    }
+    if (!json) { return NULL; }
 
     // Get the chat_id field
     cJSON *chat_id_item = cJSON_GetObjectItem(json, "chat_id");
-    if (!cJSON_IsString(message_item)) {
-        return NULL;
-    }
+    if (!cJSON_IsString(chat_id_item)) { return NULL; }
 
     // Duplicate the chat_id string to return (remember to free it later)
-    return strdup(message_item->valuestring);
+    return strdup(chat_id_item->valuestring);
 }
-
