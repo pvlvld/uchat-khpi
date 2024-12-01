f78cfe8d7b641391fe9936e4bddf4b2fd2bfd20c
Vlad Pavlenko <pavlenkoliar@gmail.com>
2024-12-01 19:49:28 +0200

    fix: missed null include & formatting

--- server/src/utils/chat_id_validation.c
+++ server/src/utils/chat_id_validation.c
@@ -1,15 +1,13 @@
-#include <stdbool.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include <time.h>
 
 bool is_valid_chat_id(const char *chat_id_str) {
-    if (chat_id_str == NULL || *chat_id_str == '\0') {
-        return false;
-    }
+    if (chat_id_str == NULL || *chat_id_str == '\0') return false;
 
     for (int i = 0; chat_id_str[i] != '\0'; i++) {
-        if (!isdigit(chat_id_str[i])) {
-            return false; // If any character is not a digit, it's not valid
-        }
+        // If any character is not a digit, it's not valid
+        if (!isdigit(chat_id_str[i])) return false;
     }
 
     return true;
