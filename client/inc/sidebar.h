7ed71c4a091f1651eda8acafcbfbf5c1d225265e
roman <osenny.roman@gmail.com>
2024-12-07 19:16:21 +0200

    add message_receipt in sidebar.h

--- client/inc/sidebar.h
+++ client/inc/sidebar.h
@@ -12,7 +12,7 @@ GtkWidget *sidebar_init(void);
 GtkWidget *sidebar_create_avatar(t_chat_info *chat_info);
 GtkWidget *sidebar_create_chatblock(t_chat_info *chat_info);
 GtkWidget *sidebar_create_bottom(void);
-// void message_receipt(GtkWidget *widget, ssize_t index, t_messages_struct *message_struct);
+void message_receipt(GtkWidget *widget, ssize_t index, t_messages_struct *message_struct);
 void update_chatblock(GtkWidget *event_box, t_chat_info *chat_info, int is_new);
 
 typedef struct {
